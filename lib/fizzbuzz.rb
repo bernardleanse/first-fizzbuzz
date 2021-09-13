9008a00ee41f1d853898ec1baaa65be40c8b69d2
KNtanos <86077849+Kntanos@users.noreply.github.com>
2021-09-13 17:50:39 +0100

    refactored
    Co-authored-by: Bernard <bernardleanse96@gmail.com>
    Co-authored-by: Kostas <kostasntanos@hotmail.com>

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,19 @@
 # Should return fizz if divisible by 3 
 # buzz if divisible by 5
 # fizzbuzz if divisible by both
-
+=begin
 def fizzbuzz(number)
   return "fizz" if number % 3 == 0 && number % 5 != 0
   return "buzz" if number % 5 == 0 && number % 3 != 0
   return "fizzbuzz" if number % 3 == 0 && number % 5 == 0
   return number
 end 
+=end
 
+def fizzbuzz(number)
+
+  return 'fizzbuzz' if number % 3 == 0 && number % 5 == 0
+    
+  number % 5 == 0 ? 'buzz' : number % 3 == 0 ? 'fizz' : number
+
+end
